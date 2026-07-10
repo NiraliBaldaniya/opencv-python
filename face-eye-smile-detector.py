6d9fb0f02260896bf4c0c30de134ff96931cfcfb
NiraliBaldaniya <niralibaldaniya17@gmail.com>
2026-07-10 17:10:32 +0530

    add face detector

---
+++ face-eye-smile-detector.py
@@ -0,0 +1,44 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+smile_cascade = cv2.CascadeClassifier("haarcascade_smile.xml")
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Could not open webcam.")
+    exit()
+
+while True:
+
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+
+    for (x,y,w,h) in faces:
+
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
+
+        roi_gray = gray[y:y+h, x:x+w]
+        
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        smiles = smile_cascade.detectMultiScale(roi_gray)
+        
+        if len(eyes) >= 2 :
+            cv2.putText(frame, "eye detected", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 2)
+
+        if len(smiles) >= 1:
+            cv2.putText(frame, "smile detected", (x, y-30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 2)
+
+    cv2.imshow("Face Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
